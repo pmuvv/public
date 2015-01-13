f608945c4bd8844cd2c1da5faaec3521b9d19a31
pmuvv <pmuvv@yandex.ru>
2015-01-13 11:53:43 +0600

    Update public.c

--- public.c
+++ public.c
@@ -1,52 +1,41 @@
-//структура 
-     
-typedef struct
+class Abc
 {
-    int         NA;     //Номер суток данных альманаха
-    int         N4;     //Номер четырехлетнего периода
-    int       Trcv;     //Время приема альманаха по ШВ АРН
-    int         KP;     //Признак ожидаемой секундной коррекции,б/р.
-    double    TAUc;     //Поправка к шкале времени системы ГЛОНАСС относительно UTC(SU),с.
-    double  TAUgps;     //Поправка на расхождение системных шкал времени GPS(T_GPS) и ГЛОНАСС (Т_GLN),c
-    double      B1;     //Коэффициент для определения DUT1,c
-    double      B2;     //Коэффициент для определения DUT1,c/сутки
-    gln_alm_nka_t NKA[GLN_N];//Альманахи НКА
-} gln_alm_t;
-
-class param_box             // абстрактный класс
-{
-public:
-    virtual void get() = 0; // виртуальный метод получения информации
-    virtual void put() = 0; // виртуальный метод добавления информации
 private:
-    bool flag = 0;
+    int* x;
+    int size;
+public:
+    Abc(int num){x = new int[num]; size = num;}
+    ~Abc(){delete[] x;}
+    void take(int *a, int num);
+    void put();
 };
 
-// реализация класса gln_alm_box
-class gln_alm_box : public param_box
+void Abc::take(int *a, int num)
 {
-public:
-    void get();                     //данный метод, должен возвращать данные типа gln_alm_t
-    void put(gln_alm_t *inp_alm);   //этот должен возвращать какой-то флаг.
-private:
-    gln_alm_t alm_gln;
-};
-
-gln_alm_t gln_alm_box::get()
-{
-    if(flag == 0)
+    if(num >= 0 && num <= size)
     {
-        //flag = true;
-        return alm_gln;
+        x[num] = a;
+    }
+}
+void Abc::put()
+{
+    for(int i=0;i<size;i++)
+    {
+        std::cout << "x[" << i << "] = " << x[i] << std::endl;
     }
 }
 
-gln_alm_box::put(gln_alm_t *inp_alm)
+double scal_vect(double *x, double *y);
+
+int main()
 {
-    if(flag == 0)
-    {
-        flag = true;
-        alm_gln = inp_alm;
-        flag = false;
-    }
+    Abc *t = new Abc(5);
+    int inp = 10;
+    t->take(&inp, 0);
+    t->take(&inp, 1);
+    t->take(&inp, 2);
+    t->take(&inp, 3);
+    t->put();
+
+    return 0;
 }
