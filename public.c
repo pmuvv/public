562fb63bcece3d474626b5db4b8612c97e5e7b1f
pmuvv <pmuvv@yandex.ru>
2015-06-03 14:52:03 +0600

    Update public.c

--- public.c
+++ public.c
@@ -1,41 +1,44 @@
+struct Data
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+}
+
 class Abc
 {
-private:
-    int* x;
-    int size;
 public:
-    Abc(int num){x = new int[num]; size = num;}
-    ~Abc(){delete[] x;}
-    void take(int *a, int num);
-    void put();
+    Abc(){}
+    ~Abc(){}
+    void setBcaPtr(){
+        Bca* bca = new Bca();
+        for(/* какое-то условие */)
+            QList<Data*>  listDataPtr= /* далее код запроса указателей на Data из бд */;
+        bca->setData(listDataPtr);
+    }
+    
+    void setBcaPtr(){
+        Bca* bca = new Bca();
+        for(/* какое-то условие */)
+            QList<Data>  listData= /* далее код запроса объектов Data из бд */;
+        bca->setData(listData);
+    }
 };
 
-void Abc::take(int *a, int num)
+class Bca
 {
-    if(num >= 0 && num <= size)
-    {
-        x[num] = a;
-    }
+public:
+    Bca(){};    //constructor
+    ~Bca(){};   //destructor
+    setData(QList<Data*> &data);    // setter ptr
+    setData(QList<Data> &data);     // setter obj
+    /* далее не важно */
 }
-void Abc::put()
-{
-    for(int i=0;i<size;i++)
-    {
-        std::cout << "x[" << i << "] = " << x[i] << std::endl;
-    }
-}
-
-double scal_vect(double *x, double *y);
 
 int main()
 {
-    Abc *t = new Abc(5);
-    int inp = 10;
-    t->take(&inp, 0);
-    t->take(&inp, 1);
-    t->take(&inp, 2);
-    t->take(&inp, 3);
-    t->put();
 
     return 0;
 }
